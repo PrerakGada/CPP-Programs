8a648d5a59dc52caccce8c58ed7cc497dce16407
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-09-16 14:00:09 +0530

    tut4

---
+++ tut4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int glo = 6;
+int main(){
+    int a = 5;
+    int b = 7;
+    cout<<"The value of a is "<<a<<"\nThe value of b is "<<b<<endl;
+    cout<<glo<<endl;
+    return 0;
+} 
