27d1adf1a8e8c5eed8a9bd72ee0fdae9e83e82f9
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-09-17 12:46:59 +0530

    startup

--- tut5.cpp
+++ tut5.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    int num1, num2;
+    cout<<"Enter the value of num1: "; // << is Insertion Operator
+    cin>>num1; // >> is extraction Operator
+
+    cout<<"Enter the value of num2: ";
+    cin>>num2;
+
+    cout<<"Sum is "<<num1+num2;
 
     return 0;
-}
+} 
